a04827b9a1a0be1fa62a610793e35c8fda864f3e
avicha <avichabc@gmail.com>
2017-01-10 18:07:46 +0800

    feat:增加了下载media到qiniu的服务

--- controllers/routes.py
+++ controllers/routes.py
@@ -8,6 +8,7 @@ from controllers.medical_kit_instance_setting import medical_kit_instance_settin
 def init_app(current_app):
     # 服务
     service_blueprint.add_url_rule('/get_weixin_jsapi_params', 'get_weixin_jsapi_params_api', ServiceController.get_weixin_jsapi_params, methods=['get'])
+    service_blueprint.add_url_rule('/download_weixin_media', 'download_weixin_media_api', ServiceController.download_weixin_media, methods=['get'])
     current_app.register_blueprint(service_blueprint, url_prefix='/api/service')
     # 管理员
     medical_kit_instance_blueprint.add_url_rule('/get', 'get_api', MedicalKitInstanceController.get, methods=['get'])
--- controllers/service.py
+++ controllers/service.py
@@ -2,9 +2,32 @@
 from flask import Blueprint
 
 from backend_common.controllers.service import ServiceController as ServiceCommonController
+from backend_common.middlewares.request_service import get_request_params
+import env
 
 service_blueprint = Blueprint('service', __name__)
 
+from backend_common.services.weixin_api import WeixinAPI
+from backend_common.config import weixin as weixin_api_config
+weixin_api = WeixinAPI(weixin_api_config)
+
+from qiniu import Auth, put_file, etag, urlsafe_base64_encode
+from backend_common.config import qiniu as qiniu_config
+
 
 class ServiceController(ServiceCommonController):
-    pass
+
+    @classmethod
+    @get_request_params()
+    def download_weixin_media(cls, data):
+        media_id = data.get('media_id')
+        result = weixin_api.get_media(media_id)
+        file_path = env.STORAGE_FILE_DIR + '/weixin_media_'+media_id+'.amr'
+        with open(file_path) as f:
+            f.write(result)
+        # 构建鉴权对象
+        q = Auth(qiniu_config.access_key, qiniu_config.secret_key)
+        # 要上传的空间
+        bucket_name = 'weixin-media-resource'
+        # 生成上传 Token，可以指定过期时间等
+        token = q.upload_token(bucket_name, media_id, 3600)
