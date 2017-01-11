f33768196905bc0f4a1a3c25d150f96b4211a854
avicha <avichabc@gmail.com>
2017-01-11 15:59:01 +0800

    feat:增加了保存药箱设置的功能

--- controllers/medical_kit_instance.py
+++ controllers/medical_kit_instance.py
@@ -3,8 +3,11 @@ from flask import Blueprint
 
 from backend_common.controllers.medical_kit_instance import MedicalKitInstanceController as MedicalKitInstanceCommonController
 from backend_common.middlewares.request_service import get_request_params
+from backend_common.models.database import database
 from backend_common.models.medical_kit import MedicalKit as MedicalKitModel
 from backend_common.models.medical_kit_instance import MedicalKitInstance as MedicalKitInstanceModel
+from backend_common.models.medical_kit_instance_setting import MedicalKitInstanceSetting as MedicalKitInstanceSettingModel
+from backend_common.models.medical_kit_instance_box_setting import MedicalKitInstanceBoxSetting as MedicalKitInstanceBoxSettingModel
 
 medical_kit_instance_blueprint = Blueprint('medical_kit_instance', __name__)
 
@@ -24,3 +27,19 @@ class MedicalKitInstanceController(MedicalKitInstanceCommonController):
             return cls.success_with_result(medical_kit_instance)
         else:
             raise MedicalKitInstanceModel.NotFoundError('找不到该药箱')
+
+    @classmethod
+    @get_request_params()
+    def set_setting(cls, data):
+        medical_kit_instance_id = data.get('medical_kit_instance_id')
+        setting = data.get('setting')
+        box_settings = data.get('box_settings')
+        with database.transaction():
+            num = MedicalKitInstanceSettingModel.update(prompt_sound=setting.get('prompt_sound')).where(MedicalKitInstanceSettingModel.medical_kit_instance_id == medical_kit_instance_id).execute()
+            if not num:
+                MedicalKitInstanceSettingModel.create(medical_kit_instance_id=medical_kit_instance_id, prompt_sound=setting.get('prompt_sound'))
+            for box_setting in box_settings:
+                num = MedicalKitInstanceBoxSettingModel.update(medical_name=box_setting.get('medical_name'), medical_barcode=box_setting.get('medical_barcode'), schedule_times=','.join(box_setting.get('schedule_times')), piece_per_time=box_setting.get('piece_per_time'), unit=box_setting.get('unit')).where(MedicalKitInstanceBoxSettingModel.medical_kit_instance_id == medical_kit_instance_id, MedicalKitInstanceBoxSettingModel.box_index == box_setting.get('box_index')).execute()
+                if not num:
+                    MedicalKitInstanceBoxSettingModel.create(medical_kit_instance_id=medical_kit_instance_id, box_index=box_setting.get('box_index'), medical_name=box_setting.get('medical_name'), medical_barcode=box_setting.get('medical_barcode'), schedule_times=','.join(box_setting.get('schedule_times')), piece_per_time=box_setting.get('piece_per_time'), unit=box_setting.get('unit'))
+            return cls.success_with_result(None)
--- controllers/routes.py
+++ controllers/routes.py
@@ -10,12 +10,10 @@ def init_app(current_app):
     service_blueprint.add_url_rule('/get_weixin_jsapi_params', 'get_weixin_jsapi_params_api', ServiceController.get_weixin_jsapi_params, methods=['get'])
     service_blueprint.add_url_rule('/download_weixin_media', 'download_weixin_media_api', ServiceController.download_weixin_media, methods=['get'])
     current_app.register_blueprint(service_blueprint, url_prefix='/api/service')
-    # 管理员
+    # 药箱实例
     medical_kit_instance_blueprint.add_url_rule('/get', 'get_api', MedicalKitInstanceController.get, methods=['get'])
+    medical_kit_instance_blueprint.add_url_rule('/set_setting', 'set_setting_api', MedicalKitInstanceController.set_setting, methods=['post'])
     current_app.register_blueprint(medical_kit_instance_blueprint, url_prefix='/api/medical_kit_instance')
     # 药品
     medical_blueprint.add_url_rule('/scan', 'scan_api', MedicalController.scan, methods=['get'])
     current_app.register_blueprint(medical_blueprint, url_prefix='/api/medical')
-    # 药箱盒子设置
-    medical_kit_instance_setting_blueprint.add_url_rule('/create', 'create_api', MedicalKitInstanceSettingController.create, methods=['post'])
-    current_app.register_blueprint(medical_kit_instance_setting_blueprint, url_prefix='/api/medical_kit_instance_setting')
